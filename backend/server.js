6cd25b0b9b8fe5b078d7d76bebdd71a0452fb59a
Gungun1308 <Guptagungun1308@gmail.com>
2026-05-14 13:53:49 +0530

    fixed mobile upload limits

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,8 @@ connectDB();
 app.use(cors());
 
 // Body parser to handle JSON data
-app.use(express.json({ extended: false }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // --- Routes ---
 // Mount all API routes
